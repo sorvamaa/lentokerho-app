abe1c7b0a3ad7dae3f9c00bc5a1ff873ad5f9aa1
sorvamaa <marko.sorvamaa@gmail.com>
2026-04-07 09:10:32 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const express = require('express'); 
+const express = require('express');  
 const session = require('express-session');
 const pgSession = require('connect-pg-simple')(session);
 const helmet = require('helmet');
